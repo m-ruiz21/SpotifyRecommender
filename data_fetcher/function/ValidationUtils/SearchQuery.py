346ce358cd315117e627861952902185e5a56857
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-11-12 01:15:08 -0600

    refactoring af

--- data_fetcher/function/ValidationUtils/SearchQuery.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-from Models.Result import Result
-
-def get_search_query(req: func.HttpRequest) -> Result[str, str]:
-    query = req.route_params.get('task_param')
-    if query:
-        return Result.Ok(query)
-    else:
-        return Result.Err(f"Failed to retreive search query. Please provide a valid search query")
